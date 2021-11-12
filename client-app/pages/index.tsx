165adff11fd669ec65355af3cfcae691e71d606e
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-11-12 14:27:38 +0100

    ✅Init next project

---
+++ client-app/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Header from '../components/header'
+
+
+const Home: NextPage = () => {
+  return (
+    <div className=''>
+     <Header/>
+    </div>
+  )
+}
+
+export default Home
