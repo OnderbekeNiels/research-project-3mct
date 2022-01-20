2b8ef7bcefb67533b5279798d6f4cdfdc13aedcd
OnderbekeNiels <55786916+OnderbekeNiels@users.noreply.github.com>
2022-01-20 12:04:44 +0100

    ☑️ Analized bundle

--- client-app/next.config.js
+++ client-app/next.config.js
@@ -2,9 +2,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({ reactStrictMode: true });
 
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+// };
